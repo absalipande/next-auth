6eca179cca78afe73c120f72897156b2e468a85f
Amiel Brencis Salipande <absalipande@gmail.com>
2024-04-19 17:56:40 +0800

    feat: Implemented CardWrapper

---
+++ app/auth/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className='flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-800 to-zinc-950'>{children}</div>;
+};
+
+export default AuthLayout;
