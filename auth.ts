ad3f949ebeb535edc207e782e8549f682ccb9cea
Amiel Brencis Salipande <absalipande@gmail.com>
2024-08-28 12:19:47 +0800

    feat: Integrated Resend for sending emails

--- auth.ts
+++ auth.ts
@@ -21,6 +21,18 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     },
   },
   callbacks: {
+    async signIn({ user, account }) {
+      // allow OAuth without email verification
+      if (account?.provider !== 'credentials') return true;
+
+      // prevent signIn without email verification
+      const existingUser = await getUserById(user.id as string);
+      if (!existingUser?.emailVerified) return false;
+
+      // TODO: Add 2FA check
+
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
