d37ec710611c35474ae4a201c9fde5daeca78fd5
Amiel Brencis Salipande <absalipande@gmail.com>
2024-09-03 11:18:34 +0800

    feat: Created a server action for resetting the password

--- actions/reset.ts
+++ actions/reset.ts
@@ -1,25 +1,28 @@
-'use server'
+'use server';
 
-import * as z from 'zod'
+import * as z from 'zod';
 
-import { ResetSchema } from "@/schemas"
-import { getUserByEmail } from "@/data/user"
+import { ResetSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
+import { sendPasswordResetEmail } from '@/lib/mail';
+import { generatePasswordResetToken } from '@/lib/tokens';
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
-  const validatedFields = ResetSchema.safeParse(values)
+  const validatedFields = ResetSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: 'Invalid email!' }
+    return { error: 'Invalid email!' };
   }
 
-  const { email } = validatedFields.data
+  const { email } = validatedFields.data;
 
-  const existingUser = await getUserByEmail(email)
+  const existingUser = await getUserByEmail(email);
   if (!existingUser) {
-    return { error: 'Email does not exist!' }
+    return { error: 'Email does not exist!' };
   }
 
-  // TODO: Send password reset email
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(passwordResetToken.email, passwordResetToken.token);
 
-  return { success: 'Password reset email sent!' }
-}
+  return { success: 'Password reset email sent!' };
+};
