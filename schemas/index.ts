772e7651f9c63b4c2bb4bde9ccb53dadac79e8e9
Amiel Brencis Salipande <absalipande@gmail.com>
2024-09-04 11:45:14 +0800

    feat: Created a new password schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,17 +1,23 @@
 import * as z from 'zod';
 
-export const LoginSchema = z.object({
-  email: z.string().email({
-    message: 'Email is required',
-  }),
-  password: z.string().min(1, {
-    message: 'Password is required',
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: 'Minimum of 6 characters required',
   }),
 });
 
 export const ResetSchema = z.object({
   email: z.string().email({
     message: 'Email is required',
+  }),
+});
+
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  password: z.string().min(1, {
+    message: 'Password is required',
   }),
 });
 
