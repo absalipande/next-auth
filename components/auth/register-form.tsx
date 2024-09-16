55af16181fe63f9724430ae327a4a2d1bc274b36
Amiel Brencis Salipande <absalipande@gmail.com>
2024-09-16 11:36:33 +0800

    small formatting fix

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -16,97 +16,11 @@ import { FormError } from '@/components/form-error';
 import { FormSuccess } from '@/components/form-succeess';
 import { register } from '@/actions/register';
 
-// export const RegisterForm = () => {
-//   const [error, setError] = useState<string | undefined>('');
-//   const [success, setSuccess] = useState<string | undefined>('');
-//   const [isPending, startTransition] = useTransition();
-
-//   const form = useForm<z.infer<typeof RegisterSchema>>({
-//     resolver: zodResolver(RegisterSchema),
-//     defaultValues: {
-//       email: '',
-//       password: '',
-//       name: '',
-//     },
-//   });
-
-//   const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
-//     setError('');
-//     setSuccess('');
-
-//     startTransition(() => {
-//       register(values).then((data) => {
-//         setError(data.error);
-//         setSuccess(data.success);
-//       });
-//     });
-//   };
-
-//   return (
-//     <CardWrapper headerLabel='Create an account' backButtonLabel='Already have an account?' backButtonHref='/auth/login' showSocial>
-//       <Form {...form}>
-//         <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
-//           <div className='space-y-4'>
-//             <FormField
-//               control={form.control}
-//               name='name'
-//               render={({ field }) => (
-//                 <FormItem>
-//                   <FormLabel>Name</FormLabel>
-//                   <FormControl>
-//                     <Input {...field} disabled={isPending} placeholder='John Doe' />
-//                   </FormControl>
-//                   <FormMessage />
-//                 </FormItem>
-//               )}
-//             />
-
-//             <FormField
-//               control={form.control}
-//               name='email'
-//               render={({ field }) => (
-//                 <FormItem>
-//                   <FormLabel>Email</FormLabel>
-//                   <FormControl>
-//                     <Input {...field} disabled={isPending} placeholder='yourname@example.com' type='email' />
-//                   </FormControl>
-//                   <FormMessage />
-//                 </FormItem>
-//               )}
-//             />
-
-//             <FormField
-//               control={form.control}
-//               name='password'
-//               render={({ field }) => (
-//                 <FormItem>
-//                   <FormLabel>Password</FormLabel>
-//                   <FormControl>
-//                     <Input {...field} disabled={isPending} placeholder='******' type='password' />
-//                   </FormControl>
-//                   <FormMessage />
-//                 </FormItem>
-//               )}
-//             />
-//           </div>
-
-//           <FormError message={error} />
-//           <FormSuccess message={success} />
-
-//           <Button type='submit' disabled={isPending} className='w-full'>
-//             Create an account
-//           </Button>
-//         </form>
-//       </Form>
-//     </CardWrapper>
-//   );
-// };
-
 export const RegisterForm = () => {
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   const [isPending, startTransition] = useTransition();
-  const [showPassword, setShowPassword] = useState(false); // Password visibility state
+  const [showPassword, setShowPassword] = useState(false);
 
   const form = useForm<z.infer<typeof RegisterSchema>>({
     resolver: zodResolver(RegisterSchema),
@@ -170,18 +84,9 @@ export const RegisterForm = () => {
                   <FormLabel>Password</FormLabel>
                   <FormControl>
                     <div className='relative'>
-                      <Input
-                        {...field}
-                        disabled={isPending}
-                        placeholder='******'
-                        type={showPassword ? 'text' : 'password'} // Toggle between text and password
-                      />
-                      <button
-                        type='button'
-                        onClick={() => setShowPassword(!showPassword)} // Toggle password visibility
-                        className='absolute right-3 top-2 text-gray-500'
-                      >
-                        {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />} 
+                      <Input {...field} disabled={isPending} placeholder='******' type={showPassword ? 'text' : 'password'} />
+                      <button type='button' onClick={() => setShowPassword(!showPassword)} className='absolute right-3 top-2 text-gray-500'>
+                        {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
                       </button>
                     </div>
                   </FormControl>
