ddbdbdd986c9a642fe37ebd874f7fd3da0119207
Amiel Brencis Salipande <absalipande@gmail.com>
2024-09-17 11:54:38 +0800

    feat: Edited the LoginForm UI to include an input for the two auth code

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -22,6 +22,7 @@ export const LoginForm = () => {
   const searchParams = useSearchParams();
   const urlError = searchParams.get('error') === 'OAuthAccountNotLinked' ? 'Email already in use with different provider!' : '';
 
+  const [showTwoFactor, setShowTwoFactor] = useState(false);
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   const [showPassword, setShowPassword] = useState(false);
@@ -40,10 +41,22 @@ export const LoginForm = () => {
     setSuccess('');
 
     startTransition(() => {
-      login(values).then((data) => {
-        setError(data?.error);
-        setSuccess(data?.success);
-      });
+      login(values)
+        .then((data) => {
+          if (data?.error) {
+            form.reset();
+            setError(data.error);
+          }
+          if (data?.success) {
+            form.reset();
+            setSuccess(data.success);
+          }
+
+          if (data?.twoFactor) {
+            setShowTwoFactor(true);
+          }
+        })
+        .catch(() => setError('Something went wrong'));
     });
   };
 
@@ -52,50 +65,69 @@ export const LoginForm = () => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
           <div className='space-y-4'>
-            <FormField
-              control={form.control}
-              name='email'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input {...field} disabled={isPending} placeholder='yourname@example.com' type='email' />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {showTwoFactor && (
+              <FormField
+                control={form.control}
+                name='code'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Two Factor Code</FormLabel>
+                    <FormControl>
+                      <Input {...field} disabled={isPending} placeholder='123456' />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            {!showTwoFactor && (
+              <>
+                <FormField
+                  control={form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input {...field} disabled={isPending} placeholder='yourname@example.com' type='email' />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-            <FormField
-              control={form.control}
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <div className='relative'>
-                      <Input {...field} disabled={isPending} placeholder='******' type={showPassword ? 'text' : 'password'} />
-                      <button type='button' onClick={() => setShowPassword(!showPassword)} className='absolute right-3 top-2 text-gray-500'>
-                        {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
-                      </button>
-                    </div>
-                  </FormControl>
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className='relative'>
+                          <Input {...field} disabled={isPending} placeholder='******' type={showPassword ? 'text' : 'password'} />
+                          <button type='button' onClick={() => setShowPassword(!showPassword)} className='absolute right-3 top-2 text-gray-500'>
+                            {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
+                          </button>
+                        </div>
+                      </FormControl>
 
-                  <Button size='sm' variant='link' asChild className='px-0 font-normal'>
-                    <Link href='/auth/reset'>Forgot password?</Link>
-                  </Button>
+                      <Button size='sm' variant='link' asChild className='px-0 font-normal'>
+                        <Link href='/auth/reset'>Forgot password?</Link>
+                      </Button>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
           </div>
 
           <FormError message={error || urlError} />
           <FormSuccess message={success} />
 
           <Button type='submit' disabled={isPending} className='w-full'>
-            Login
+            {showTwoFactor ? 'Confirm' : 'Login'}
           </Button>
         </form>
       </Form>
