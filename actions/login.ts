d627dd4cadcc8016fe0ca08861ac24563d473db2
Amiel Brencis Salipande <absalipande@gmail.com>
2024-09-17 11:24:32 +0800

    feat: Further improved the two auth factor feature by sending the code to the email

--- actions/login.ts
+++ actions/login.ts
@@ -6,9 +6,9 @@ import { AuthError } from 'next-auth';
 import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-import { generateVerificationToken } from '@/lib/tokens';
+import { generateVerificationToken, generateTwoFactorToken } from '@/lib/tokens';
 import { getUserByEmail } from '@/data/user';
-import { sendVerificationEmail } from '@/lib/mail';
+import { sendVerificationEmail, sendTwoFactorTokenEmail } from '@/lib/mail';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -29,6 +29,13 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
     return { success: 'Confirmation email sent!' };
+  }
+
+  if (existingUser.isTwoFactorEnabled && existingUser.email) {
+    const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+    await sendTwoFactorTokenEmail(twoFactorToken.email, twoFactorToken.token);
+
+    return { twoFactor: true };
   }
 
   try {
