cb15d395f42e09aa551fd841880f70b80d4d212b
Amiel Brencis Salipande <absalipande@gmail.com>
2024-09-19 13:17:55 +0800

    feat: Added a user button and navbar for different path

---
+++ components/auth/user-button.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { FaUser } from 'react-icons/fa';
+import { ExitIcon } from '@radix-ui/react-icons';
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { LogoutButton } from '@/components/auth/logout-button';
+
+export const UserButton = () => {
+  const user = useCurrentUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || ''} />
+          <AvatarFallback className='bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-800 to-zinc-950'>
+            <FaUser className='text-white' />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-40' align='end'>
+        <LogoutButton>
+          <DropdownMenuItem className='cursor-pointer'>
+            <ExitIcon className='h-4 w-4 mr-2' />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
