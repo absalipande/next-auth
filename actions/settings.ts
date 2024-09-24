eef7a5397163e8fcfead6022df3aa5ebaea96511
Amiel Brencis Salipande <absalipande@gmail.com>
2024-09-24 10:24:45 +0800

    feat: Created a server action for settings

---
+++ actions/settings.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import * as z from 'zod';
+
+import { db } from '@/lib/db';
+import { SettingsSchema } from '@/schemas';
+import { getUserById } from '@/data/user';
+import { currentUser } from '@/lib/auth';
+
+export const settings = async (values: z.infer<typeof SettingsSchema>) => {
+  const user = await currentUser();
+  if (!user) {
+    return { error: 'Unauthorized' };
+  }
+
+  const dbUser = await getUserById(user.id as string);
+  if (!dbUser) {
+    return { error: 'User not found!' };
+  }
+
+  await db.user.update({
+    where: { id: dbUser.id },
+    data: {
+      ...values,
+    },
+  });
+
+  return { success: 'Settings Updated!' };
+};
