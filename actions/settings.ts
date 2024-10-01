ea532a880dd1bff90469bf19687febad3960da59
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-01 10:25:54 +0800

    feat: Added feature for changing the email and password for credential accounts

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,11 +1,14 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 
 import { db } from '@/lib/db';
-import { SettingsSchema } from '@/schemas';
-import { getUserById } from '@/data/user';
 import { currentUser } from '@/lib/auth';
+import { SettingsSchema } from '@/schemas';
+import { sendVerificationEmail } from '@/lib/mail';
+import { generateVerificationToken } from '@/lib/tokens';
+import { getUserByEmail, getUserById } from '@/data/user';
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -18,11 +21,37 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     return { error: 'User not found!' };
   }
 
+  // the reason for this is we need to hash the new password again, but we can't do that with OAuth accounts
   if (user.isOAuth) {
     values.email = undefined;
     values.password = undefined;
     values.newPassword = undefined;
     values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: 'Email already in use!' };
+    }
+
+    const verificationToken = await generateVerificationToken(values.email);
+
+    await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+    return { success: 'Verification email sent!' };
+  }
+
+  // for password
+  if (values.password && values.newPassword && dbUser.password) {
+    const passwordsMatch = await bcrypt.compare(values.password, dbUser.password);
+    if (!passwordsMatch) {
+      return { error: 'Invalid password!' };
+    }
+
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
+    values.password = hashedPassword;
+    values.newPassword = undefined;
   }
 
   await db.user.update({
